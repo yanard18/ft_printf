5c23cd146b462d1f7142e7b1b5aea5b3aa94bbb0
mek <mek@localhost.localdomain>
2026-02-26 01:05:54 +0300

    fix %p and %s flag for NULL address

--- Tester/ft_printf_test.c
+++ Tester/ft_printf_test.c
@@ -87,10 +87,20 @@ int	main(void)
 	TEST("2A", ft_printf("%X", 42));
 	TEST("0", ft_printf("%X", 0));
 
-	dprintf(saved_stdout, "\ntest %%p:\n");
-	int x = 0;
-	TEST_STDOUT_FUNC(printf("%p", &x), ft_printf("%p", &x));
-	TEST_STDOUT_FUNC(printf("%p", 0), ft_printf("%p", 0));
+	{
+		dprintf(saved_stdout, "\ntest %%p:\n");
+		int x = 0;
+		TEST_STDOUT_FUNC(printf("%p", &x), ft_printf("%p", &x));
+	}
+
+	{
+		dprintf(saved_stdout, "\ntest (nil):\n");
+		TEST_STDOUT_FUNC(printf("%d", 0), ft_printf("%d", 0)); // expect 0
+		TEST_STDOUT_FUNC(printf("%x", 0), ft_printf("%x", 0)); // expect 0
+		TEST_STDOUT_FUNC(printf("%s", 0), ft_printf("%s", 0)); // expect (nil)
+		TEST_STDOUT_FUNC(printf("%p", 0), ft_printf("%p", 0)); // expect (nil)
+
+	}
 
 dprintf(saved_stdout, "\ntest %%%%:\n");
 	TEST("%", ft_printf("%%"));
