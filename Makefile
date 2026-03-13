4ebe5ecec1566f64c8c33be2e231e179a0acf580
Deniz Doga Yanar <dyanar@k1m03s06.42istanbul.com.tr>
2026-03-13 03:46:57 +0300

    add bonus files

--- Makefile
+++ Makefile
@@ -8,6 +8,12 @@ SRCS        = $(addprefix $(SRC_DIR), ft_printf.c token.c token_helper.c \
               convert_base.c convert_flags.c convert_padding.c)
 OBJS        = $(SRCS:.c=.o)
 
+BONUS_DIR	= src_bonus/
+BONUS_SRCS  = $(addprefix $(BONUS_DIR), ft_printf_bonus.c token_bonus.c token_helper_bonus.c \
+              token_utils_bonus.c token_validate_bonus.c token_sort_bonus.c str_utils_bonus.c \
+              convert_base_bonus.c convert_flags_bonus.c convert_padding_bonus.c)
+BONUS_OBJS  = $(BONUS_SRCS:.c=.o)
+
 LIBFT_DIR	= libft/
 LIBFT		= $(LIBFT_DIR)libft.a
 
@@ -28,6 +34,17 @@ $(NAME): $(LIBFT) $(OBJS)
 	SAVE
 	END
 	EOF
+
+.ONESHELL:
+bonus: $(LIBFT) $(BONUS_OBJS)
+	ar -M << EOF
+	CREATE $(NAME)
+	ADDLIB $(LIBFT)
+	ADDMOD $(BONUS_OBJS)
+	SAVE
+	END
+	EOF
+
 libft:
 	make -C ./libft
 $(LIBFT):
---
+++ src_bonus/token_bonus.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyanar <dyanar@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/11 20:09:27 by dyanar            #+#    #+#             */
+/*   Updated: 2026/03/11 20:19:59 by dyanar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static void	free_token(void *content)
+{
+	t_token	*token;
+
+	token = (t_token *)content;
+	if (!token)
+		return ;
+	if (token->type == WIDTH || token->type == PRECISION)
+	{
+		if (token->value)
+		{
+			free(token->value);
+			token->value = NULL;
+		}
+	}
+}
+
+static t_list	*tokenize(const char **format, t_token *g_token_buf)
+{
+	t_list	*lst;
+	t_token	*out_token;
+
+	lst = NULL;
+	while (*++(*format))
+	{
+		is_token(**format, g_token_buf, &out_token);
+		if (out_token)
+		{
+			push_token(&lst, out_token);
+			if (out_token->type == PRECISION)
+				(*format)++;
+			if (out_token->type == PRECISION || out_token->type == WIDTH)
+				int_to_token(format, out_token);
+			else if (out_token->type == SPECIFIER)
+			{
+				(*format)++;
+				break ;
+			}
+		}
+		else
+			return (lst);
+	}
+	return (lst);
+}
+
+static char	*apply_specifier(t_list *lst, va_list args)
+{
+	t_token	*token;
+
+	token = (t_token *)ft_lstlast(lst)->content;
+	if (token->value[0] == '%')
+		return (ft_strdup("%"));
+	return (token->f(&(void *){va_arg(args, void *)}, lst));
+}
+
+char	*eval_next_token(t_list **lst, t_list *start_lst, char *s)
+{
+	t_token	*token;
+
+	token = (t_token *)((*lst)->content);
+	if (token && token->type != SPECIFIER && token->f)
+		s = token->f(s, start_lst);
+	*lst = (*lst)->next;
+	return (s);
+}
+
+char	*read_token(const char **format, va_list args, t_token *g_token_buf)
+{
+	t_list		*lst;
+	t_list		*start_lst;
+	int			valid;
+	char		*s;
+	const char	*rewind_str_addr;
+
+	rewind_str_addr = *format;
+	lst = tokenize(format, g_token_buf);
+	start_lst = lst;
+	valid = validate_tokens(lst);
+	if (!valid)
+	{
+		ft_lstclear(&lst, free_token);
+		*format = rewind_str_addr + 1;
+		g_token_buf[16].type = DOWNGRADE;
+		if (**format == '\0')
+			return (NULL);
+		return (ft_strdup("%"));
+	}
+	s = apply_specifier(lst, args);
+	sort_tokens(&lst);
+	while (lst)
+		s = eval_next_token(&lst, start_lst, s);
+	ft_lstclear(&start_lst, free_token);
+	return (s);
+}
